0d092160473e235f94efb76cb403382ac296e589
saharbavi <saharbavi04@gmail.com>
2025-05-22 18:24:38 +0330

    upload first file

---
+++ course_info.py
@@ -0,0 +1,117 @@
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter.messagebox as msg
+
+from course_manager import *
+from validator import course_validator
+
+person_list = read_from_file("course.dat")
+
+def load_data(course_list):
+    course_list = read_from_file("course.dat")
+    for row in table.get_children():
+        table.delete(row)
+
+    for course in course_list:
+        table.insert("", END, values=course)
+##to do: ERROR
+def reset_form():
+    course_id.set(len(course_list) + 1)
+    course_name.set("")
+    course_code.set(0)
+    course_day.set("")
+    course_date.set("")
+    teacher_name.set("")
+    load_data(course_list)
+
+##to do: ERROR
+def save_btn_click():
+    person = (course_id.get(), course_name.get(), course_code.get(), course_day.get(),course_date.get(), teacher_name.get())
+    errors = course_validator(person)
+    if errors:
+        msg.showerror("Errors", "\n".join(errors))
+    else:
+        msg.showinfo("Saved", "course saved")
+        course_list.append(course)
+        write_to_file("course.dat", course_list)
+        reset_form()
+#
+#
+# def table_select(x):
+#     selected_person = table.item(table.focus())["values"]
+#     if selected_person:
+#         id.set(selected_person[0])
+#         name.set(selected_person[1])
+#         family.set(selected_person[2])
+#         account.set(selected_person[3])
+#
+#
+# def edit_btn_click():
+#     pass
+#
+#
+# def remove_btn_click():
+#     pass
+
+
+window = Tk()
+window.title("Course Info")
+window.geometry("997x270")
+
+# course id
+Label(window, text="Course Id:").place(x=20, y=20)
+course_id = IntVar(value=1)
+Entry(window, textvariable=course_id, state="readonly").place(x=80, y=20)
+
+# Course Name
+Label(window, text="Course Name:").place(x=20, y=60)
+course_name = StringVar()
+Entry(window, textvariable=course_name).place(x=100, y=60)
+
+# course code
+Label(window, text="Course Code:").place(x=20, y=90)
+course_code = IntVar()
+Entry(window, textvariable=course_code).place(x=100, y=90)
+
+# Course Day
+Label(window, text="Course Day:").place(x=20, y=120)
+course_day = StringVar()
+Entry(window, textvariable=course_day).place(x=100, y=120)
+
+# Course Date
+Label(window, text="Course Date:").place(x=20, y=150)
+course_date = StringVar()
+Entry(window, textvariable=course_date).place(x=100, y=150)
+
+# Teacher
+Label(window, text="Teacher Name:").place(x=20, y=185)
+teacher_name = StringVar()
+Entry(window, textvariable=teacher_name).place(x=112, y=185)
+
+table = ttk.Treeview(window, columns=[1, 2, 3, 4, 5, 6], show="headings")
+table.heading(1, text="Course Id")
+table.heading(2, text="Course Name")
+table.heading(3, text="Course Code")
+table.heading(4, text="Course Day")
+table.heading(5, text="Course Date ")
+table.heading(6, text="Teacher")
+
+table.column(1, width=60)
+table.column(2, width=100)
+table.column(3, width=100)
+table.column(4, width=100)
+table.column(5, width=100)
+table.column(6, width=100)
+
+# table.bind("<<TreeviewSelect>>", table_select)
+
+table.place(x=245, y=20)
+
+Button(window, text="Save", width=6, command=save_btn_click).place(x=20, y=220)
+Button(window, text="Edit", width=6, command=edit_btn_click).place(x=90, y=220)
+Button(window, text="Remove", width=6, command=remove_btn_click).place(x=160, y=220)
+Button(window, text="Clear", width=6, command=reset_form).place(x=20, y=180, width=190)
+
+reset_form()
+
+window.mainloop()
---
+++ validator.py
@@ -0,0 +1,18 @@
+import re
+
+def course_validator(course):
+    errors = []
+    if not (type(course[0]) == int and course[0]>0):
+        errors.append('Course ID must be an integer > 0')
+
+    if not (type(course[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[1])):
+        errors.append('Course Name is Invalid')
+
+
+    if not (type(course[2]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[2])):
+        errors.append('Course Code is Invalid')
+
+    if not (type(course[3]) == int and course[3]>0):
+        errors.append('Person Account Amount must be an integer > 0')
+
+    return errors
