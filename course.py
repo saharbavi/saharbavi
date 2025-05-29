34acbe255e1c6e1c41bd40a71f428ddc89888d7e
saharbavi <saharbavi04@gmail.com>
2025-05-29 18:25:54 +0330

    edit_btn have ERROR

--- course.py
+++ course.py
@@ -1,2 +1,17 @@
 class Course:
-    pass
+
+    def __init__(self, c_id: int, title: str, code: str,teacher_name:str, day: str):
+        self.c_id = c_id
+        self.title = title
+        self.code = code
+        self.teacher_name = teacher_name
+        self.day = day
+
+
+    def save(self):
+        print(f"{self.title} {self.code} has been saved.")
+
+
+course1=Course(1,"python","201","sara","mon")
+
+Course.save(course1)
