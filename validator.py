3cef5f73145f12cf0961b5062958f2ea8d70ea9b
saharbavi <saharbavi04@gmail.com>
2025-05-29 19:34:59 +0330

    course.py

--- validator.py
+++ validator.py
@@ -1,27 +1,24 @@
 import re
-from collections.abc import coroutine
-
-from course import *
 
 
 def course_validator(course):
     errors = []
-    if not (type(course[0]) == int and course.course_id > 0):
+    if not (type(course[0]) == int and course[0] > 0):
         errors.append('Course ID must be an integer > 0')
 
-    if not (type(course[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.course_name)):
+    if not (type(course[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[1])):
         errors.append('Course Name is Invalid')
 
-    if not (type(course[2]) == str and re.match(r"^\d{3}$", course.course_code)):
+    if not (type(course[2]) == str and re.match(r"^\d{3}$", course[2])):
         errors.append('Course Code is Invalid')
 
-    if not (type(course[3]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.course_day)):
+    if not (type(course[3]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[3])):
         errors.append('Course day is Invalid')
 
-    if not (type(course[4]) == str and re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$",course.course_date)):
+    if not (type(course[4]) == str and re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", course[4])):
         errors.append('Course date is Invalid')
 
-    if not (type(course[5]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.teacher_name)):
+    if not (type(course[5]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[5])):
         errors.append('Teacher name is Invalid')
 
     return errors
