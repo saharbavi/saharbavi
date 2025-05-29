76907e1aad7acec8e9987cd271fbda7bd4a40303
saharbavi <saharbavi04@gmail.com>
2025-05-29 19:30:14 +0330

    course.py

--- course.py
+++ course.py
@@ -1,17 +1,21 @@
+from  validator import *
+
 class Course:
 
-    def __init__(self, c_id: int, title: str, code: str,teacher_name:str, day: str):
-        self.c_id = c_id
-        self.title = title
-        self.code = code
+    def __init__(self, course_id: int, course_name: str, course_code: str, course_day: str, course_date: str,
+                 teacher_name: str):
+        self.course_id = course_id
+        self.course_name = course_name
+        self.course_code = course_code
+        self.course_day = course_day
+        self.course_date = course_date
         self.teacher_name = teacher_name
-        self.day = day
-
 
     def save(self):
-        print(f"{self.c_id} {self.title} {self.code} {self.teacher_name} {self.day} has been saved.")
+
+        print(f"{self.course_id} {self.course_name} {self.course_code} {self.course_day} {self.course_date} {self.teacher_name}  has been saved.")
 
 
-course1=Course(1,"python","201","sara","mon")
-
+course1 = Course(1, "python", "201", "mon", "1404-07-12" , "sara")
+print(course_validator(course1))
 Course.save(course1)
--- validator.py
+++ validator.py
@@ -1,24 +1,27 @@
 import re
+from collections.abc import coroutine
+
+from course import *
 
 
 def course_validator(course):
     errors = []
-    if not (type(course[0]) == int and course[0] > 0):
+    if not (type(course[0]) == int and course.course_id > 0):
         errors.append('Course ID must be an integer > 0')
 
-    if not (type(course[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[1])):
+    if not (type(course[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.course_name)):
         errors.append('Course Name is Invalid')
 
-    if not (type(course[2]) == str and re.match(r"^\d{3}$", course[2])):
+    if not (type(course[2]) == str and re.match(r"^\d{3}$", course.course_code)):
         errors.append('Course Code is Invalid')
 
-    if not (type(course[3]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[3])):
+    if not (type(course[3]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.course_day)):
         errors.append('Course day is Invalid')
 
-    if not (type(course[4]) == str and re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", course[4])):
+    if not (type(course[4]) == str and re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$",course.course_date)):
         errors.append('Course date is Invalid')
 
-    if not (type(course[5]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course[5])):
+    if not (type(course[5]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", course.teacher_name)):
         errors.append('Teacher name is Invalid')
 
     return errors
