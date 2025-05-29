e9a2143df4a790aa31b876fbfb4546f106ec9aaa
saharbavi <saharbavi04@gmail.com>
2025-05-29 18:18:51 +0330

    edit_btn have ERROR

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from course import Course
 from teacher import Teacher
 
 
 
-
+course1=Course(1,"python","201","sara","mon")
 teach1=Teacher(12,"sara","alipour","1998")
 
-Teacher.save(teach1.name,teach1.family)
+Teacher.save(teach1)
+Course.save(course1)
